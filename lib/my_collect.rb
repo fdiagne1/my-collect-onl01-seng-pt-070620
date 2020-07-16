31fa6e5116154912a0ace3ea93e2170bc0bf47e3
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-16 20:44:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,15 @@
+def my_collect(students) 
+  my_collect(students) do |student|
+    student.split(" ").first
+end
+end
+
 def my_collect(array)
   i = 0
   collection = []
   while i < array.length
   collection << yield(array[i])
   i += 1
-end
-end
-
-def my_collect(students) 
-  my_collect(students) do |student|
-    student.split(" ").first
 end
 end
 
